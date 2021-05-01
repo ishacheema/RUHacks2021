fc43e96d0c0acfde993b0d3857b3d0667755c5d8
ishacheema <60581256+ishacheema@users.noreply.github.com>
2021-05-01 19:21:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,26 @@ bad_words = ["shut up", "stupid", "dumb", "idiot", "loser"]
 starter_encouragements = [
   "Cheer up!",
   "Hang in there.",
-  "You are a great person {message.author}"
+  "You are a great person"
 ]
 
-insult_warning = [
-  "Please dont send bad words in the channel"
-]
+
+insult_warning = 'Please dont send bad words in the channel!'
+
 
 # teacher = '837914922872602664'
 student = '837915203680600065'
 
-@client.event
-async def on_message(message):
-    msg = message.content
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice(starter_encouragements))
-    if any(word in msg for word in sad_words):
-      await message.channel.send(insult_warning)
+# @client.event
+# async def on_message(message):
+#     msg = message.content
+#     if any(word in msg for word in sad_words):
+#       await message.channel.send(random.choice(starter_encouragements))
+    
+    # if any(word in msg for word in sad_words):
+    #   await message.channel.send(insult_warning)
+def is_it_me(ctx):
+    return ctx.author.id == 400822001930993674
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -44,12 +47,37 @@ async def inspire(message):
     quote = get_quote()
     await message.channel.send(quote)
 
+
 @client.event
 async def on_ready():
     print("the bot is ready")
 
-def is_it_me(ctx):
-    return ctx.author.id == 400822001930993674
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    msg = message.content
+    
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(starter_encouragements))
+    if any(word in msg for word in bad_words):
+      await message.channel.send(insult_warning)
+
+    # if message.author.id == 400822001930993674:
+    #   if msg.startswith("$new"):
+    #     encouraging_message = msg.split("$new ",1)[1]
+    #     update_encouragements(encouraging_message)
+    #     await message.channel.send("New encouraging message added.")
+
+
+
+# @client.command()
+# @commands.check(is_it_me)
+# async def newencouragement(ctx):
+#     encouraging_message = msg.split("$new ",1)[1]
+#     update_encouragements(encouraging_message)
+#     await message.channel.send("New encouraging message added.")
+    
+
 
 @client.command()
 @commands.check(is_it_me)
